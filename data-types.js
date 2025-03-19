9101d957a98829b1a02ebafd4f633d215de5a1e1
Damilare2007 <damilaredaniel922@gmail.com>
2025-03-19 19:24:14 +0100

    Data type lessons

---
+++ data-types.js
@@ -0,0 +1,26 @@
+// data type string
+let Studentsname = "Teejay";
+console.log(Studentsname);
+
+const tag = "Damilare is a great guy";
+console.log(tag);
+
+let boss = "17";
+console.log(boss);
+
+
+//data type number
+
+let age = 17;
+console.log(age);
+
+const price = 5000;
+console.log(price);
+
+//data type boolean
+
+let DamiSister = true;
+console.log=(DamiSister);
+
+let IsStudent = false;
+console.log=(IsStudent);
